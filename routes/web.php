8dee8f284b350c46ca4902f1ca5b7ab8b76768b0
Navruzbekashurov <navruzashurov0321@gmail.com>
2025-04-21 02:37:30 +0500

    tekshirish

--- routes/web.php
+++ routes/web.php
@@ -6,4 +6,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about' , App\Http\Controllers\Controller::class);
+Route::get('/about' , App\Http\Controllers\Controller::class)->name();
