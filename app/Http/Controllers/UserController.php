94e9d340b7d2570fe53668126badd0c12dbe792c
bkozimov <bobur.kozimov@gmail.com>
2025-04-22 10:08:48 +0500

    fixing

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,7 +8,6 @@ use App\Http\Requests\StoreUserRequest;
 use App\Http\Requests\UpdateUserRequest;
 use App\Models\User;
 use App\Services\UserService;
-use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
