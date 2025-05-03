55c59c8e6ac0529947e72a5d245cf68d55279b16
Navruzbekashurov <navruzashurov0321@gmail.com>
2025-05-03 16:44:58 +0500

    add functions

--- app/Http/Controllers/RestaurantController.php
+++ app/Http/Controllers/RestaurantController.php
@@ -2,12 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Restaurant;
+
 class RestaurantController extends Controller
 {
     public function index()
     {
+        $restaurants = Restaurant::all();
 
-        return view('/rest');
+        return view('restaurants.index', compact('restaurants'));
+
+    }
+
+    public function store()
+    {
 
     }
 
