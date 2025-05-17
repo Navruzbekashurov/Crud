59e7efd02362785fafd5c3b05351f6a8fbac6c51
Navruzbekashurov <navruzashurov0321@gmail.com>
2025-05-18 03:21:10 +0500

    add functions

--- app/Models/BranchWorkTime.php
+++ app/Models/BranchWorkTime.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class BranchWorkTime extends Model
 {
+    protected $fillable = [
+        'branch_id',
+        'day',
+        'open_time',
+        'close_time',
+        'day_off'
+    ];
+
     public function branch(): BelongsTo
     {
         return $this->belongsTo(Branch::class);
